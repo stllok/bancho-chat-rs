4fa8b7e7cac0b2b78ef7891c472dcb5cdfb7a1f6
Kelvin Leung <cl20103B11@yahoo.com.hk>
2024-02-06 00:21:16 +0800

    template constructing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="grid h-screen grid-cols-4">
+	<div class="m-3 flex flex-col gap-3 self-baseline">
+		{#each [123, 321, '+'] as e}
+			<button class="rounded-full border-2 bg-gray-100 p-3 text-center hover:bg-gray-400"
+				>{e}</button
+			>
+		{/each}
+	</div>
+	<div class="col-span-3 grid grid-rows-10 bg-white">
+		<div class="row-span-9 flex flex-col bg-gray-400">
+			<p>test: hello world!</p>
+			<p>test: XD</p>
+		</div>
+		<div class="flex">
+			<input type="text" class="grow bg-gray-500 text-2xl" />
+			<button class="bg-green-500 px-5">{'=>'}</button>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.purple.800);
+	}
+</style>
